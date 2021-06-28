82d1e46e35daa041621384a118aa3b080e6ebb98
TeresaJanaszek <teresa.janaszek@gmail.com>
2021-06-28 21:00:04 +0200

    Finished contact form with validation, popup and printing information in console

--- form.js
+++ form.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', setup);
-
 const get = document.getElementById.bind(document);
 const query = document.querySelector.bind(document);
 
@@ -7,20 +6,30 @@ function setup() {
   
   let modalRoot = get('modal-root');
   let button = get('submitButton');
+  let form = get('contactForm')
   let modal = query('.modal');
   let closeButton = get('closeButton')
+
+  let name = get('nameInput')
+  let surname= get('surnameInput')
+  let number= get('phoneInput')
+  let email= get('mailInput')
+  let message= get('messageInput')
   
   modalRoot.addEventListener('click', rootClick);
-  button.addEventListener('click', openModal);
   modal.addEventListener('click', modalClick);
   closeButton.addEventListener('click', rootClick)
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(`${name.value} ${surname.value}`)
+    console.log(`phone number: ${number.value}`)
+    console.log(`e-mail: ${email.value}`)
+    console.log(`${message.value}`)
+    modalRoot.classList.add('visible');
+  })
 
   function rootClick() {
     modalRoot.classList.remove('visible');
-  }
-  
-  function openModal() {
-    modalRoot.classList.add('visible');
   }
   
   function modalClick(e) {
